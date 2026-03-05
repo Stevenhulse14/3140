dff745c184f5006384546a36907f44002d42d3b6
steven <shotta3xd@gmail.com>
2026-03-05 12:05:11 -0500

    added content

---
+++ Week_05/demo/backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+console.log(process.env.PORT);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
